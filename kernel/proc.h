7e438ade1b84f93bd83c755bad50c3f1276d0624
fadyadell <fady3718@gmail.com>
2025-05-21 13:41:24 +0300

    change3

--- kernel/proc.h
+++ kernel/proc.h
@@ -85,7 +85,6 @@ struct trapframe {
   /* 272 */ uint64 t5;
   /* 280 */ uint64 t6;
 };
-
 enum procstate { UNUSED, USED, SLEEPING, RUNNABLE, RUNNING, ZOMBIE };
 
 // Per-process state
@@ -99,21 +98,25 @@ struct proc {
   int xstate;                  // Exit status to be returned to parent's wait
   int pid;                     // Process ID
 
+  struct proc *parent;         // Parent process
+
+  // Scheduling metrics
+  uint creation_time;          // When the process was created
+  uint end_time;               // When the process exited
+  uint run_time;               // Total time spent running on CPU
+  uint waiting_time;           // Total time spent in RUNNABLE state
+  int priority;                // For priority scheduling
+
+  // Process memory and context
   uint64 kstack;               // Virtual address of kernel stack
   uint64 sz;                   // Size of process memory (bytes)
   pagetable_t pagetable;       // User page table
   struct trapframe *trapframe; // data page for trampoline.S
   struct context context;      // swtch() here to run process
+
+  // File and directory info
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
+
   char name[16];               // Process name (debugging)
-};
-struct proc {
-  ...
-  uint creation_time;   // When the process was created
-  uint end_time;        // When the process exited
-  uint run_time;        // Total time spent running on CPU
-  uint waiting_time;    // Total time spent in RUNNABLE state
-  int priority;         // For priority scheduling
-  ...
 };
